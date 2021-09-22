0960c50a5dd5d311fe265bde84a7c74d501287df
Twigonometry <mac.j.goodwin@gmail.com>
2021-09-22 22:27:25 +0100

    Change from rot13 to base64

--- web/app.py
+++ web/app.py
@@ -1,13 +1,10 @@
 from flask import Flask
 from flask import render_template, jsonify, make_response, redirect, request
 from flask_bootstrap import Bootstrap
+from base64 import b64encode, b64decode
 
 app = Flask(__name__)
 Bootstrap(app)
-
-rot13 = str.maketrans(
-    'ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz',
-    'NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm')
 
 # global password
 password = "SESH2021You'llNeverGuess!"
@@ -26,20 +23,22 @@ def verify_login():
     #they must guess they need to modify it
     if request.form.get('password') == password:
         print("Correct!")
-        resp.set_cookie('status', "authorised".translate(rot13))
+        resp.set_cookie('status', b64encode(b"authorised"))
     else:
-        resp.set_cookie('status', "unauthorised".translate(rot13))
+        resp.set_cookie('status', b64encode(b"unauthorised"))
 
     return resp
 
 @app.route("/secret")
 def view_secret():
     if request.cookies.get('status') is not None:
-        if request.cookies.get('status').translate(rot13) == "authorised":
-            #unset the cookie for the next player
+        if b64decode(request.cookies.get('status')) == "authorised":
+            #unset the cookie and reset the password for the next player
             #then return the page
             resp = make_response(render_template('secret.html'))
             resp.set_cookie('status', 'none', max_age=0)
+            global password
+            password = "SESH2021You'llNeverGuess!"
             return resp
         else:
             return redirect('/', code=302)
